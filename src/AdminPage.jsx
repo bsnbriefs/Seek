919f59ae62803f8a756792ce3a01c968e5908427
Barrister Street <bsnbriefs@gmail.com>
2026-09-05 10:40:20 +0100

    Update AdminPage.jsx

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -404,8 +404,9 @@ async function updateOfferStatus(id, status) {
                       </span>
                     )}
                   </div>
-                  ))
+                  ))}
                 </div>
+                )}
               {/* OFFERS */}
 <div className="mt-10">
   <h2 className="text-2xl font-semibold mb-4">Offers</h2>
